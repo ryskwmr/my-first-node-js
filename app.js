00dd9a16c5822afc09b7363520fce0e9d06853e9
Kawamura Ryosuke <ryosuke.k0104@gmail.com>
2020-06-20 19:51:22 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 'use strict';
+
+const { nullLiteral } = require("babel-types");
+
 const number = process.argv[2] || 0;
 let sum = 0;
 for (let i = 1; i <= number; i++) {
   sum = sum + i;
 }
 console.log(sum);
+
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
